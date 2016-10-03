b8954b88630f1e53f51c639b130e4f0d2fe28031
Noah Osterhout <osterhoutn@gmail.com>
2016-10-03 14:14:35 -0400

    Create Osterhout_Ales_ProjectOne.py

---
+++ Osterhout_Python/Osterhout_Ales_ProjectOne.py
@@ -0,0 +1,72 @@
+#	Programmer: Noah Osterhout | Keegan Ales
+#	Date: October 3rd 2016 1:36PM EST
+#	Project: Osterhout_Ales_ProjectOne.py
+
+# Project Escaped
+
+print('Carlos said, \'It\'s not the end of the world if we don\'t make it to Chicago by 4:30.')
+print()
+print("Ben Franklin once said, \"A penny a saved is a penny earned.\" ")
+print()
+print("1\n2")
+print()
+print("Make sure to save work to you Z:\\ Drive before you leave.")
+print()
+print()
+print()
+
+
+# Project Space Ages
+
+nameOne = input("What is your name? ")
+nameTwo = input("What is your first teachers name? ")
+nameThree = input("What is your second teachers name? ")
+
+print()
+
+ageOne = int(input("What is your age? "))
+ageTwo = int(input("What is your first teachers age? "))
+ageThree = int(input("What is your second teachers age? "))
+
+print()
+
+print(nameOne, ageOne)
+print(nameTwo, ageTwo)
+print(nameThree, ageThree)
+
+print()
+
+merc_age_one = ageOne / .241
+merc_age_two = ageTwo / .241
+merc_age_three = ageThree / .241
+
+venus_age_one = ageOne / .615
+venus_age_two = ageTwo / .615
+venus_age_three = ageThree / .615
+
+print(nameOne, "%.2f" % merc_age_one)
+print(nameTwo, "%.2f" % merc_age_two)
+print(nameThree, "%.2f" % merc_age_three)
+
+print()
+
+print(nameOne, "%.2f" % venus_age_one)
+print(nameTwo, "%.2f" % venus_age_two)
+print(nameThree, "%.2f" % venus_age_three)
+
+print()
+
+# Project Basic Calc
+
+
+
+
+
+
+
+
+
+
+
+
+
